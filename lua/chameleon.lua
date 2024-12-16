f4c3f49088cf28779cf7abf1feb288e0dd09b288
JunaidQrysh <junaidqrysh@gmail.com>
2024-12-16 18:42:24 +0530

    Refactor with improvements

--- lua/chameleon.lua
+++ lua/chameleon.lua
@@ -1,44 +1,43 @@
 local M = {}
 
 function M.setup()
-	require("which-key").add({
-		{
-			"<leader>sC",
-			function()
-				require("chameleon.nui").choose_theme()
-			end,
-			desc = "Colorscheme",
-		},
-	})
+  require("which-key").add({
+    {
+      "<leader>sC",
+      function()
+        require("chameleon.nui").choose_theme()
+      end,
+      desc = "Colorscheme",
+    },
+  })
 
-	require("which-key").add({
-		{
-			"<leader>sT",
-			function()
-				require("chameleon.utils").ToggleHyde()
-			end,
-			desc = "Toggle-Hyde",
-		},
-	})
+  require("which-key").add({
+    {
+      "<leader>sT",
+      function()
+        require("chameleon.utils").ToggleHyde()
+      end,
+      desc = "Toggle-Hyde",
+    },
+  })
 
-	local file = io.open(vim.fn.stdpath("data") .. "/theme", "r")
-	if file then
-		M.hyde = false
-		vim.g.nt = file:read("*a")
-		file:close()
-	else
-		M.hyde = true
-		vim.g.nt = require("chameleon.hyprdots").get_theme_from_hypr() or "Tokyo-Night"
-		local file_path = require("chameleon.utils").chameleon_path .. "/colorschemes/" .. vim.g.nt .. ".lua"
-		local file1 = io.open(file_path, "r")
-		if file1 then
-			file1:close()
-		else
-			vim.g.nt = "Tokyo-Night"
-		end
-	end
-	require("chameleon.nui").change_theme(vim.g.nt)
-	require("chameleon.utils").load_all_highlights()
+  local file = io.open(vim.fn.stdpath("data") .. "/theme", "r")
+  if file then
+    M.hyde = false
+    vim.g.nt = file:read("*a")
+    file:close()
+  else
+    M.hyde = true
+    vim.g.nt = require("chameleon.hyprdots").get_theme_from_hypr() or "Tokyo-Night"
+    local file_path = require("chameleon.utils").chameleon_path .. "/colorschemes/" .. vim.g.nt .. ".lua"
+    local file1 = io.open(file_path, "r")
+    if file1 then
+      file1:close()
+    else
+      vim.g.nt = "Tokyo-Night"
+    end
+  end
+  require("chameleon.utils").load_all_highlights()
 end
 
 return M
--- lua/chameleon/hyprdots.lua
+++ lua/chameleon/hyprdots.lua
@@ -1,25 +1,25 @@
 local M = {}
 
 function M.get_theme_from_hypr()
-	local hypr_conf_path = os.getenv("HOME") .. "/.config/hypr/themes/theme.conf"
-	local hypr_conf_file = io.open(hypr_conf_path, "r")
+  local hypr_conf_path = os.getenv("HOME") .. "/.config/hypr/themes/theme.conf"
+  local hypr_conf_file = io.open(hypr_conf_path, "r")
 
-	if hypr_conf_file then
-		local hypr_conf_content = hypr_conf_file:read("*all")
-		hypr_conf_file:close()
+  if hypr_conf_file then
+    local hypr_conf_content = hypr_conf_file:read("*all")
+    hypr_conf_file:close()
 
-		---@type string
-		local extracted_theme
+    ---@type string
+    local extracted_theme
 
-		for line in hypr_conf_content:gmatch("[^\n]+") do
-			if line:match("^%$GTK_THEME%s*=%s*") then
-				extracted_theme = line:match("%$GTK_THEME%s*=%s*(.+)")
-				extracted_theme = extracted_theme:gsub("^%s*", ""):gsub("%s*$", "") -- Trim whitespace
-				break
-			end
-		end
-		return extracted_theme
-	end
+    for line in hypr_conf_content:gmatch("[^\n]+") do
+      if line:match("^%$GTK_THEME%s*=%s*") then
+        extracted_theme = line:match("%$GTK_THEME%s*=%s*(.+)")
+        extracted_theme = extracted_theme:gsub("^%s*", ""):gsub("%s*$", "") -- Trim whitespace
+        break
+      end
+    end
+    return extracted_theme
+  end
 end
 
 return M
