1055b4b5b331547fb363b213e008e0ff063b225f
JunaidQrysh <junaidqrysh@gmail.com>
2025-11-28 18:47:05 +0530

    Fix first nvim startup theme loading

--- plugin/init.lua
+++ plugin/init.lua
@@ -1,13 +1,26 @@
 vim.g.base46_cache = vim.fn.stdpath "data" .. "/base46/"
 local ok, _ = pcall(dofile, vim.g.base46_cache .. "term")
+
 if not ok then
   vim.g.nt = "catppuccin"
   require("base46").load_all_highlights()
-  vim.schedule(function()
-    require("plenary.reload").reload_module "base46"
-    require("base46").load_all_highlights()
-  end)
+
+  local timer = vim.loop.new_timer()
+  timer:start(
+    200,
+    200,
+    vim.schedule_wrap(function()
+      if vim.v.vim_did_enter == 1 then
+        timer:stop()
+        timer:close()
+
+        require("plenary.reload").reload_module "base46"
+        require("base46").load_all_highlights()
+      end
+    end)
+  )
 end
+
 if vim.loop.fs_stat(vim.fn.stdpath "data" .. "/hyde") ~= nil then
   require("chameleon.hyprdots").set_hyde()
 end
